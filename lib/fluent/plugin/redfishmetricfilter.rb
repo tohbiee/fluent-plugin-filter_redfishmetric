3231a24109049b31996c400b4463e1817e1ffc67
tohbiee <tobiajagbe@microsoft.com>
2021-03-10 18:14:51 -0600

    func update and test added

---
+++ lib/fluent/plugin/redfishmetricfilter.rb
@@ -0,0 +1,45 @@
+require 'fluent/plugin/filter'
+
+module Fluent::Plugin
+  class RedfishMetricFilter < Filter
+    # Register this filter as "passthru"
+    Fluent::Plugin.register_filter('redfishmetric', self)
+
+    # config_param works like other plugins
+
+    def configure(conf)
+      super
+      # Do the usual configuration here
+    end
+
+    # def start
+    #   super
+    #   # Override this method if anything needed as startup.
+    # end
+
+    # def shutdown
+    #   # Override this method to use it to free up resources, etc.
+    #   super
+    # end
+
+    def customredfishmetricfilter(record)
+      metricValueList = record["MetricValues"]
+      res = []
+      metricValueList.each do |val|
+          myRecord = {}
+          myRecord["Namespace"] = "ColomanagerFluentdRedfish"
+          myRecord["Metric"] = val["MetricValue"]
+          label = val["Oem"]["Dell"]["Label"]
+          myRecord["Value"] = label.delete(val["MetricId"]).strip()
+          myRecord["Report"] = record["Id"]
+          myRecord["Dimension"] = {"Region" => "CentralusEUAP", "IP" => record["REMOTE_ADDR"]}
+          res << myRecord
+      end
+      res
+  end
+
+    def filter(tag, time, record)
+        customredfishmetricfilter(record)
+    end
+  end
+end
---
+++ test/test_redfishmetricfilter.rb
@@ -0,0 +1,92 @@
+require_relative 'helper'
+require 'fluent/plugin/test/driver/filter'
+require 'fluent/plugin/redfishmetricfilter'
+
+class redfishmetricfilterTest < Test::Unit::TestCase
+    include Fluent
+
+    def setup 
+        Fluent::Test.setup
+    end
+
+    # default configuration for tests
+    CONFIG = %[
+        param1 value1
+        param2 value2
+      ]
+
+    def create_driver(conf = '')
+        Fluent::Test::Driver::Filter.new(Fluent::Plugin::RedfishMetricFilter).configure(conf)
+    end
+
+    def filter(config, messages)
+        d = create_driver(config)
+        d.run(default_tag: 'input.access') do
+            messages.each do |message|
+                d.feed(message)
+            end
+        end
+        d.filtered_records
+    end
+
+    #sub_test_case 'configure' do
+    sub_test_case 'configured with invalid configuration' do
+        test 'empty configuration' do
+            assert_raise(Fluent::ConfigError) do
+                create_driver('')
+            end
+        end
+        
+        test 'param1 should reject too short string' do
+            conf = %[
+                param1 a
+            ]
+            assert_raise(Fluent::ConfigError) do
+               create_driver(conf)
+            end
+        end
+    end
+
+    sub_test_case 'plugin will add some fields' do
+        test 'filter metric and add dimension to record' do
+            conf = CONFIG
+
+            messages = [
+                {
+                    "@odata.type" => "#MetricReport.v1_2_0.MetricReport",
+                    "@odata.context" => "/redfish/v1/$metadata#MetricReport.MetricReport",
+                    "@odata.id" => "/redfish/v1/TelemetryService/MetricReports/MemorySensor",
+                    "Id" => "MemorySensor",
+                    "Name" => "Memory Sensor Metric Report Definition",
+                    "ReportSequence" => "2",
+                    "MetricReportDefinition" => {
+                        "@odata.id" => "/redfish/v1/TelemetryService/MetricReportDefinitions/MemorySensor"
+                    },
+                    "Timestamp" => "2021-02-19T12:48:31-06:00",
+                    "MetricValues" => [
+                        {
+                            "MetricId" => "TemperatureReading",
+                            "Timestamp" => "2021-02-19T12:48:30-06:00",
+                            "MetricValue" => "31",
+                            "Oem" => {
+                                "Dell" => {
+                                    "ContextID" => "DIMM.Socket.A1",
+                                    "Label" => "DIMM Socket A1 TemperatureReading"
+                                }
+                            }
+                        },
+                    ]
+                }
+
+            ]
+
+            expected = [{"Namespace"=>"ColomanagerFluentdRedfish", "Metric"=>"30", "Value"=>"DIMM Sock A1", "Report"=>"MemorySensor", "Dimension"=>{"Region"=>"Ce
+                ntralusEUAP", "IP"=>nil}},]
+            
+            filtered_records = filter(conf, messages)
+            assert_equal(expected, filtered_records)
+            end
+        end
+        # ...
+    end    
+end
