76eff837833c8699f35123fe8bd929a00898f3fa
tohbiee <tobiajagbe@microsoft.com>
2021-03-31 15:58:24 -0700

    fiilter updated

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 build with gem build
     $ gem build fluent-plugin-redfishmetric.gemspec
 install with gem install
-    $ gem install fluent-plugin-redfishmetric.gem
+    $ gem install fluent-plugin-filter_redfishmetric
 
 ## Configuration
 
--- lib/fluent/plugin/filter_redfishmetric.rb
+++ lib/fluent/plugin/filter_redfishmetric.rb
@@ -5,6 +5,12 @@ module Fluent::Plugin
     # Register this filter as "redfishmetric"
     Fluent::Plugin.register_filter('redfishmetric', self)
 
+    # config_param works like other plugins
+    config_param :Namespace, :string, :default => 'ColomanagerFluentdRedfish'
+    config_param :Coloregion, :string, :default => 'CentralusEUAP'
+    config_param :Filter, :bool, :default => false
+    config_param :Metric, :Set, :default => {"TemperatureReading", "CompositeTemparature", "CriticalWarning", "Temperature", "DriveTemperature", "CRCErrorCount", "UncorrectableErrorCount", "PercentDriveLifeRemaining"}
+
     def configure(conf)
       super
       @metricValueList = []
@@ -15,13 +21,26 @@ module Fluent::Plugin
       @metricValueList = record["MetricValues"]
       res = []
       @metricValueList.each do |val|
-          res.add(tag, time, val)
+          myRecord = {}
+          myRecord["Namespace"] = @Namespace
+		      myRecord["Report"] = record["Id"]
+          label = val["Oem"]["Dell"]["Label"]
+          myRecord["Metric"] = label.delete(val["MetricId"]).strip()
+          myRecord["Value"] = val["MetricValue"]
+          myRecord["Dimension"] = {"Region" => @Coloregion, "IP" => record["REMOTE_ADDR"]}
+		      if Filter
+	            if Metric.include?(myRecord["Metric"])
+			          res.add(tag, time, myRecord)
+		      else
+              res.add(tag, time, myRecord)
+          end
+        
       end
       route.emit(res)
-  end
+    end
 
     def filter(tag, time, record)
-        customredfishmetricfilter(record)
+        customredfishmetricfilter(tag, time, record)
     end
   end
 end
