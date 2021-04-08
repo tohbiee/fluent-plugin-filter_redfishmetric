a658544eede69d711685cbbf09f4178b900c22d9
tohbiee <tobiajagbe@microsoft.com>
2021-04-08 09:59:33 -0700

    logic update

--- lib/fluent/plugin/filter_redfishmetric.rb
+++ lib/fluent/plugin/filter_redfishmetric.rb
@@ -8,38 +8,38 @@ module Fluent::Plugin
     # config_param works like other plugins
     config_param :Namespace, :string, :default => 'ColomanagerFluentdRedfish'
     config_param :Coloregion, :string, :default => 'CentralusEUAP'
-	  config_param :Filter, :bool, :default => false
-    config_param :Metric, :string, :default => "TemperatureReading"
+	config_param :Filter, :bool, :default => false
+    config_param :Metric, :array, :default => [], value_type: :string
 	
     def configure(conf)
       super
       @metricValueList = []
+      # Do the usual configuration here
 	  end
 	
-	  def filter_stream(tag, es)
+	def filter_stream(tag, es)
       new_es = Fluent::MultiEventStream.new
       es.each { |time, record|
-		    @metricValueList = record["MetricValues"]
-		    @metricValueList&.each do |val|
-		      begin
-			      myRecord = {}
-			      myRecord["Namespace"] = @Namespace
-			      myRecord["Report"] = record["Id"]
-			      myRecord["Metric"] = val["MetricId"]
-			      myRecord["Value"] = val["MetricValue"]
-			      myRecord["Dimension"] = {"Region" => @Coloregion, "IP" => record["REMOTE_ADDR"]}
-
-			      if @Filter
-			        if @Metric == val["MetricId"]
-			          new_es.add(time, myRecord)
-			        end
-			      else
+		@metricValueList = record["MetricValues"]
+		@metricValueList&.each do |val|
+		  begin
+			myRecord = {}
+			myRecord["Namespace"] = @Namespace
+			myRecord["Report"] = record["Id"]
+			myRecord["Metric"] = val["MetricId"]
+			myRecord["Value"] = val["MetricValue"]
+			myRecord["Dimension"] = {"Region" => @Coloregion, "IP" => record["REMOTE_ADDR"]}
+			if @Filter
+	          if @Metric&.include?(val["MetricId"])
+			      new_es.add(time, myRecord)
+			  end
+			else
               new_es.add(time, myRecord)
-			      end
+			end
           rescue => e
           router.emit_error_event(tag, time, record, e)
           end
-		    end
+		end
       }
       new_es
     end
