ed8a04071c3eb180a799d55b9a78a4dad015b22b
tohbiee <tobiajagbe@microsoft.com>
2021-04-13 09:30:31 -0700

    logic update

--- lib/fluent/plugin/filter_redfishmetric.rb
+++ lib/fluent/plugin/filter_redfishmetric.rb
@@ -2,44 +2,42 @@ require 'fluent/plugin/filter'
 
 module Fluent::Plugin
   class RedfishMetricFilter < Filter
-    # Register this filter as "redfishmetric"
+    # Register filter as "redfishmetric"
     Fluent::Plugin.register_filter('redfishmetric', self)
 
-    # config_param works like other plugins
+    # config_param for the plugins
     config_param :Namespace, :string, :default => 'ColomanagerFluentdRedfish'
     config_param :Coloregion, :string, :default => 'CentralusEUAP'
-	  config_param :Filter, :bool, :default => false
+    config_param :Filter, :bool, :default => false
     config_param :Metric, :array, :default => [], value_type: :string
 	
     def configure(conf)
       super
       @metricValueList = []
-      # Do the usual configuration here
 	  end
 	
-	def filter_stream(tag, es)
+	  def filter_stream(tag, es)
       new_es = Fluent::MultiEventStream.new
       es.each { |time, record|
-		@metricValueList = record["MetricValues"]
-		@metricValueList&.each do |val|
-		  begin
-			myRecord = {}
-			myRecord["Namespace"] = @Namespace
-			myRecord["Report"] = record["Id"]
-			myRecord["Metric"] = val["MetricId"]
-			myRecord["Value"] = val["MetricValue"]
-			myRecord["Dimension"] = {"Region" => @Coloregion, "IP" => record["REMOTE_ADDR"]}
-			if @Filter
-	          if @Metric&.include?(val["MetricId"])
-			      new_es.add(time, myRecord)
-			  end
-			else
+        @metricValueList = record["MetricValues"]
+        @metricValueList&.each do |val|
+          begin
+            myRecord = {}
+            myRecord["Namespace"] = @Namespace
+            myRecord["Metric"] = val["MetricId"]
+            myRecord["Dimensions"] = {"Region" => @Coloregion, "Report"=>record["Id"],"IP" => record["REMOTE_ADDR"]}
+            myRecord["Value"] = val["MetricValue"]
+            if @Filter
+              if @Metric == val["MetricId"]
+                new_es.add(time, myRecord)
+              end
+            else
               new_es.add(time, myRecord)
-			end
+            end
           rescue => e
           router.emit_error_event(tag, time, record, e)
           end
-		end
+        end
       }
       new_es
     end
